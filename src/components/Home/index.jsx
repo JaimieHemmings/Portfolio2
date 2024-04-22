5ee2c9d8e99a4d89d18f2fbc555e921e6b806630
Jaimie <jaimie.j.hemmings@gmail.com>
2024-04-22 16:02:31 +0100

    Fixed Typo

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,12 +1,12 @@
-import './index.css'
 import { Link } from 'react-router-dom'
 import ProfileImg from '../../assets/selfie.jpg'
+import './index.css'
 
 const Home = () => {
     return (
         <section className="hero-wrap">
             <div className="container">
-                <span class="subheading">Web Developer</span>
+                <span className="subheading">Web Developer</span>
                 <h1>I'm Jaimie <br />Hemmings</h1>
                 <p><Link to="/about" className="btn btn-primary">More about me &#62;</Link> <a href="/" className="btn btn-white">Hire me &#62;</a></p>
             </div>
